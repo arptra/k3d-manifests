427b6a9f935f03008d77dc6deb24ae02d0a7d6b3
a18619937 <akilyasov@sberbank.ru>
2021-11-20 20:14:28 +0300

    add cluster role

--- deploy_all.sh
+++ deploy_all.sh
@@ -3,7 +3,8 @@ k3d cluster create -p "8080:80@loadbalancer"
 #deploy dashboard
 kubectl apply -f https://raw.githubusercontent.com/arptra/k3d-manifests/master/kuber-dashboard.yaml
 #deploy argocd
-kubectl create namespace argocd
-kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+kubectl apply -n argocd -f https://raw.githubusercontent.com/arptra/k3d-manifests/master/argocd.yaml
 #deploy nginx-router
 kubectl apply -f https://raw.githubusercontent.com/arptra/k3d-manifests/master/full_router_configs.yaml
+#get token
+kubectl -n kubernetes-dashboard describe secret $(kubectl -n kubernetes-dashboard get secret | awk '/^kubernetes-dashboard-token-/{print $1}') | awk '$1=="token:"{print $2}'
