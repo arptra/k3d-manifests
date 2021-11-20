dd7d4800329331dfd66826e0441b98088e96377c
a18619937 <akilyasov@sberbank.ru>
2021-11-20 20:24:23 +0300

    add cluster role

--- deploy_all.sh
+++ deploy_all.sh
@@ -1,10 +1,13 @@
 #create cluster
 k3d cluster create -p "8080:80@loadbalancer"
 #deploy dashboard
-kubectl apply -f https://raw.githubusercontent.com/arptra/k3d-manifests/master/kuber-dashboard.yaml
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.4.0/aio/deploy/recommended.yaml
+#create sample user
+kubectl apply -f https://raw.githubusercontent.com/arptra/k3d-manifests/master/dashboard_cluster_role.yaml
 #deploy argocd
 kubectl apply -n argocd -f https://raw.githubusercontent.com/arptra/k3d-manifests/master/argocd.yaml
 #deploy nginx-router
 kubectl apply -f https://raw.githubusercontent.com/arptra/k3d-manifests/master/full_router_configs.yaml
 #get token
-kubectl -n kubernetes-dashboard describe secret $(kubectl -n kubernetes-dashboard get secret | awk '/^kubernetes-dashboard-token-/{print $1}') | awk '$1=="token:"{print $2}'
+echo "PRINT TOKEN"
+kubectl -n kubernetes-dashboard get secret $(kubectl -n kubernetes-dashboard get sa/admin-user -o jsonpath="{.secrets[0].name}") -o go-template="{{.data.token | base64decode}}"
